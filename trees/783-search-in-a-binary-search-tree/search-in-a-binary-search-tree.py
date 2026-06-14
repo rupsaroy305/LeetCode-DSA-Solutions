c13c1bd9e39e3ec6d910f298677fec294f97bbde
Rupsa Roy <rupsaroy305@gmail.com>
2026-06-14 19:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (76.31%) - LeetSync

---
+++ trees/783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchBST(self, root, val):
+        if not root:
+            return None
+        
+        if root.val == val:
+            return root
+        
+        if val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
