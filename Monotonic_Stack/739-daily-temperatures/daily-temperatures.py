ed5c51345a49ee7de2ca037f9b0c1c1139000185
Rupsa Roy <rupsaroy305@gmail.com>
2026-06-19 10:52:57 +0530

    Time: 156 ms (77.96%) | Memory: 26.8 MB (62.32%) - LeetSync

---
+++ Monotonic_Stack/739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self,temperatures):
+        n=len(temperatures)
+        res=[0] * n
+        stack=[]
+
+        for i in range(n):
+            while stack and temperatures[i]>temperatures[stack[-1]]:
+                prev=stack.pop()
+                res[prev]=i-prev
+
+            stack.append(i)
+
+        return res
+        
