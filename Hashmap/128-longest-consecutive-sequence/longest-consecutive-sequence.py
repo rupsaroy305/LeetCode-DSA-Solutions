87ccbb8aa073b910d4003fa13746def973e077a6
Rupsa Roy <rupsaroy305@gmail.com>
2026-08-12 13:21:29 +0530

    Time: 43 ms (85.21%) | Memory: 36.6 MB (36.94%) - LeetSync

---
+++ Hashmap/128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestConsecutive(self,nums):
+        s=set(nums)
+        ans=0
+        for x in s:
+            if x-1 not in s:
+                y=x
+                while y in s:
+                    y+=1
+                ans=max(ans,y-x)
+        return ans
