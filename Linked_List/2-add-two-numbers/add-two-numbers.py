99b97085f52fc922da0cd82ff8f167ac08ceb03f
Rupsa Roy <rupsaroy305@gmail.com>
2026-08-23 18:21:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (41.57%) - LeetSync

---
+++ Linked_List/2-add-two-numbers/add-two-numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addTwoNumbers(self,l1,l2):
+        dummy=ListNode(0)
+        cur=dummy
+        carry=0
+        while l1 or l2 or carry:
+            x=l1.val if l1 else 0
+            y=l2.val if l2 else 0
+            s=x+y+carry
+            carry=s//10
+            cur.next=ListNode(s%10)
+            cur=cur.next
+            if l1:
+                l1=l1.next
+            if l2:
+                l2=l2.next
+        return dummy.next
