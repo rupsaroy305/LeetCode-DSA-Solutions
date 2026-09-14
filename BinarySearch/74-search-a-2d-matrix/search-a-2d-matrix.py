bffa6186f565be8b822b74dcbe7be3cf1794a223
Rupsa Roy <rupsaroy305@gmail.com>
2026-09-14 12:00:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (41.37%) - LeetSync

--- BinarySearch/74-search-a-2d-matrix/search-a-2d-matrix.py
+++ BinarySearch/74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -3,16 +3,13 @@ class Solution:
         m=len(matrix)
         n=len(matrix[0])
         l,r=0,m*n-1
-
         while l<=r:
             mid=(l+r)//2
             x=matrix[mid//n][mid%n]
-
             if x==target:
                 return True
             elif x<target:
                 l=mid+1
             else:
                 r=mid-1
-
         return False
